b2f506bebe3e1bfe0674101037c3341916a9a23c
Josh Mcguigan <joshmcg88@gmail.com>
2019-01-19 06:25:37 -0700

    keyboard input handling

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -14,6 +14,8 @@ lazy_static! {
             idt[usize::from(TIMER_INTERRUPT_ID)]
                 .set_handler_fn(timer_interrupt_handler);
         }
+        idt[usize::from(KEYBOARD_INTERRUPT_ID)]
+            .set_handler_fn(keyboard_interrupt_handler);
         idt
     };
 }
@@ -40,10 +42,39 @@ extern "x86-interrupt" fn timer_interrupt_handler(
     unsafe { PICS.lock().notify_end_of_interrupt(TIMER_INTERRUPT_ID) }
 }
 
+extern "x86-interrupt" fn keyboard_interrupt_handler(
+    _stack_frame: &mut ExceptionStackFrame
+) {
+    use x86_64::instructions::port::Port;
+    use pc_keyboard::{Keyboard, ScancodeSet1, DecodedKey, layouts};
+    use spin::Mutex;
+
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+            Mutex::new(Keyboard::new(layouts::Us104Key, ScancodeSet1));
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    let port = Port::new(0x60);
+
+    let scancode: u8 = unsafe { port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => print!("{:?}", key),
+            }
+        }
+    }
+
+    unsafe { PICS.lock().notify_end_of_interrupt(KEYBOARD_INTERRUPT_ID) }
+}
+
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
 
 pub const TIMER_INTERRUPT_ID: u8 = PIC_1_OFFSET;
+pub const KEYBOARD_INTERRUPT_ID: u8 = PIC_1_OFFSET + 1;
 
 pub static PICS: spin::Mutex<ChainedPics> =
     spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
